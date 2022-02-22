e85821ea37464913bcf7eb7eec0118fc325c7b43
funguyz <99909775+funguyz@users.noreply.github.com>
2022-02-22 13:40:41 -0500

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -491,7 +491,7 @@ const Home = (props: HomeProps) => {
                                rel="noopener noreferrer">Shop</a></li>
                         <li><a href="https://funguy.club/whitelist" target="_blank"
                                rel="noopener noreferrer">whitelist info</a></li>
-                         <li><a href="https://funguy.club/mint" target="_blank"
+                         <li><a href="https://frontend-e6qle0m0j-funguyz1.vercel.app/" target="_blank"
                                rel="noopener noreferrer">VIP MEMBERSHIP</a></li>
                     </Menu>
                     <Wallet>
@@ -507,7 +507,7 @@ const Home = (props: HomeProps) => {
                     <DesContainer>
                         <NFT elevation={3}>
                             <h2>Funguys BASIC Membership!</h2>
-                            <h2><a href="https://frontend-mu-jade.vercel.app/" target="_blank"
+                            <h2><a href="https://frontend-e6qle0m0j-funguyz1.vercel.app/" target="_blank"
                                rel="noopener noreferrer">CLICK HERE FOR VIP NFT</a></h2>
                             <br/>
                             <div><Price
